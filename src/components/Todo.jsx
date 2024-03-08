7139fff51fa9fc0df5aa5ac880733f74a6b066ce
Alailson Barbosa <alailsonbcsilva@gmail.com>
2024-03-08 11:09:15 -0300

    finalizacao do projeto

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,20 @@
-export default function Todo({ todo }) {
+export default function Todo({ todo, removeTodo, completeTodo }) {
   return (
-    <div className="todo">
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
       <div className="content">
         <p>{todo.text}</p>
         <p className="category">({todo.category})</p>
       </div>
       <div>
-        <button className="complete">Completar</button>
-        <button className="remove">X</button>
+        <button className="complete" onClick={() => completeTodo(todo.id)}>
+          Completar
+        </button>
+        <button className="remove" onClick={() => removeTodo(todo.id)}>
+          X
+        </button>
       </div>
     </div>
   );
